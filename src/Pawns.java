32326a73428d7a52596eb9ceebf2a2c995eb6a92
Carlos Olvera <carlosolvera@wireless-10-145-64-124.public.utexas.edu>
2025-09-15 14:25:01 -0500

    inital commit

---
+++ src/Pawns.java
@@ -0,0 +1,19 @@
+public class Pawns {
+    private int value;
+    private int position;
+    public Pawns(){
+        this.position = 0;
+        this.value = 1;
+    }
+
+    public void movement(){
+        position++;
+    }
+
+    public int fight(Object opponent){
+        //based on what piece it takes
+        if(opponent instanceof ){
+            
+        }
+    }
+}
---
+++ src/mainBoard.java
@@ -0,0 +1,18 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;  // Added this
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class mainBoard {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
